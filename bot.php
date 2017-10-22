2cc3f165c1620e303e980675f623ec6bfdb2a101
Zttap <32987515+Zttap@users.noreply.github.com>
2017-10-22 19:03:05 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,7 +1,3 @@
 <?php
 echo "I am a bot";
-curl -X POST \
--H 'Content-Type:application/json' \
--H 'Authorization: Bearer {xQfWgzC/4EGg6PbukutbxPbtvQR/Y7kTiJWAAyfyFcUM+BsGQyXucITP6Vvjv3KYgO1IVXAS2nmX03cbubtTrHjfPL+cWkB0PGb81HFUw1yuZVnqGemSB/cAkeFqBCjMIolgPZQ0pYE55yBTckq/+wdB04t89/1O/w1cDnyilFU=
-}' \
-...
+
